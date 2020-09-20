845bf2c20370a04cdb4ea05c0b3648317461d82e
Nicolas Nathan Campos <camposnicolas45@gmail.com>
2020-09-20 13:32:30 -0300

    first project with nextJs

---
+++ login-system/config/emotions.ts
@@ -0,0 +1,9 @@
+/**
+ * Arquivos para configurar as coisas conforme a sua necessidade
+ */
+
+import styled, { CreateStyled } from '@emotion/styled';
+
+import theme from '../styles/theme';
+
+export default styled as CreateStyled<typeof theme>
